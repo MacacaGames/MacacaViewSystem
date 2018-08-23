44153c52a1ea4e1ebefc59a66121169606bf9f97
Miki <sableangle@gmail.com>
2018-08-23 21:03:13 +0800

    Make Editor can fit canvas size

--- ViewElementPool.cs
+++ ViewElementPool.cs
@@ -3,22 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 [ExecuteInEditMode]
-public class ViewElementPool : MonoBehaviour {
-	Canvas canvas;
-	RectTransform canvasRectTransform;
-	RectTransform rectTransform ;
-	// Use this for initialization
-	void Start () {
-		canvas = (Canvas)FindObjectOfType(typeof(Canvas));
-		canvasRectTransform = canvas.GetComponent<RectTransform>();
-		rectTransform = GetComponent<RectTransform>();
+public class ViewElementPool : MonoBehaviour
+{
+    Canvas _canvas;
+    Canvas canvas
+    {
+        get
+        {
+            if (_canvas == null)
+            {
+                _canvas = (Canvas)FindObjectOfType(typeof(Canvas));
+            }
+            return _canvas;
+        }
+    }
+    RectTransform _canvasRectTransform;
+    RectTransform canvasRectTransform
+    {
+        get
+        {
+            if (_canvasRectTransform == null)
+            {
+                _canvasRectTransform = canvas.GetComponent<RectTransform>();
+            }
+            return _canvasRectTransform;
+        }
+    }
+    RectTransform _rectTransform;
+    RectTransform rectTransform
+    {
+        get
+        {
+            if (_rectTransform == null)
+            {
+                _rectTransform = GetComponent<RectTransform>();
+            }
+            return _rectTransform;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        canvasRectTransform.ObserveEveryValueChanged(m => m.sizeDelta).Subscribe(
+            (s) =>
+            {
+                rectTransform.sizeDelta = new Vector2(s.x, s.y);
+            }
+        );
+    }
+#if UNITY_EDITOR
+    void OnGUI()
+    {
+        rectTransform.sizeDelta = new Vector2(canvasRectTransform.sizeDelta.x, canvasRectTransform.sizeDelta.y);
+    }
 
-		canvasRectTransform.ObserveEveryValueChanged(m=>m.sizeDelta).Subscribe(
-			(s)=>{
-				rectTransform.sizeDelta = new Vector2( s.x, s.y); 
-			}
-		);
-	}
-	
-	
+#endif
 }
