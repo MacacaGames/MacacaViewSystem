930a3abd79116d66e67936870b4f69c6df405668
Timo In Office <timo@cloudmacaca.com>
2020-10-13 22:00:30 +0800

    + ViewSystemLayoutRebuilder

---
+++ Runtime/Components/ViewSystemLayoutRebuilder.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CloudMacaca;
+using UnityEngine.UI;
+
+namespace CloudMacaca.ViewSystem
+{
+    [ExecuteInEditMode]
+    public class ViewSystemLayoutRebuilder : MonoBehaviour, IViewElementLifeCycle
+    {
+
+        RectTransform rectTransform
+        {
+            get
+            {
+                if (_rectTransform == null)
+                {
+                    _rectTransform = GetComponent<RectTransform>();
+                }
+                return _rectTransform;
+            }
+        }
+        RectTransform _rectTransform;
+
+        // Use this for initialization
+        public void OnBeforeShow()
+        {
+        }
+
+        public void OnBeforeLeave()
+        {
+        }
+
+        public void OnStartShow()
+        {
+            StartCoroutine(RebuildLayout());
+        }
+
+        IEnumerator RebuildLayout()
+        {
+            yield return null;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+        }
+
+        public void OnStartLeave()
+        {
+        }
+
+        public void OnChangePage(bool show)
+        {
+        }
+
+        public void OnChangedPage()
+        {
+        }
+    }
+}
