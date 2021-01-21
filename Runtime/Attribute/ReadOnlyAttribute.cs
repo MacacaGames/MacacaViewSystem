493b7ba873948ecdb5ef745681c3e452e27a8d7e
miki <sableangle@gmail.com>
2021-01-21 20:33:01 +0800

    update doc

---
+++ Runtime/Attribute/ReadOnlyAttribute.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MacacaGames
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+    }
+#endif
+}
