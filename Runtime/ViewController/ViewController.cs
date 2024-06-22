77c082aed3dafbf1cdd027f3a397d4d91cf10a5a
Miki <sableangle@gmail.com>
2024-06-22 16:46:51 +0800

    fix: changepageend may be wrong value

--- Runtime/ViewController/ViewController.cs
+++ Runtime/ViewController/ViewController.cs
@@ -553,7 +553,7 @@ namespace MacacaGames.ViewSystem
             ChangePageToCoroutine = null;
 
             //Callback
-            InvokeOnViewPageChangeEnd(this, new ViewPageEventArgs(currentViewPage, lastViewPage));
+            InvokeOnViewPageChangeEnd(this, new ViewPageEventArgs(nextViewPageForCurrentChangePage, lastViewPage));
 
             nextViewPageForCurrentChangePage = null;
             nextViewState = null;
